663fb891706375e8f60d309954e64fcb988dae84
gaserashraf <elkinggaser@gmail.com>
2021-05-12 22:04:08 +0200

    work on input

--- backend/indexer/code.java
+++ backend/indexer/code.java
@@ -1,11 +1,20 @@
-import java.util.*;  
+import java.io.IOException;
+import java.util.*; 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 public class InvertedIndex {
 
-    static class Document
+    static class DocumentClass
     {
         String[] wordInDoc; //array contains the all strings in this doc
+       
+        String title; // title of the Document
+        Vector<String> headings; // all headings of the Document
+        Vector<String> paragrahs;// all normal text of the Document
         int docIndex; // the index of this doc, ex doc 1, doc 2
-        public Document(String[] arr, int idx)
+        public DocumentClass(String[] arr, int idx)
         {
             int n = arr.length;
             wordInDoc=new String[n];
@@ -13,9 +22,15 @@ public class InvertedIndex {
                 wordInDoc[i]=arr[i];
             docIndex=idx;
         }
+        public DocumentClass()
+        {
+            headings = new Vector<String>();
+            paragrahs = new Vector<String>();
+        }
     }
     static class gaser{
         int docIndex;
+        // to do make it array of string carry the types of the singe word was mentioned ???? ???? football mentioned as a h1 and paragrah and in title
         String type;//heading normal ... should be array of Strings?
         //ex word football was a heading and paragrah
         int TF = 0;
@@ -30,13 +45,14 @@ public class InvertedIndex {
             TF=1;
         }
     }
-    static class indexer{
+    static class indexer
+    {
         HashMap<String, Vector<gaser>> wordToDocumentMap;
         indexer()
         { 
            wordToDocumentMap = new HashMap<String, Vector<gaser>>(); 
         }
-        void fillDoc(Document Doc) // o(n^2)
+        void fillDoc(DocumentClass Doc) // o(n^2)
         {
             String type = "NORMAL"; // To Do get the type 
             gaser g1 =new gaser(Doc.docIndex,type);
@@ -92,6 +108,63 @@ public class InvertedIndex {
             }
         }
     }
+   static DocumentClass htmlTODoc(String html)
+   {
+       // this function take an html document like this "<html><head><title>Sample Title</title></head>"
+       // and convert it to an document class which impemeted ???
+      DocumentClass D = new DocumentClass();
+      Document document = Jsoup.parse(html);
+      D.title=document.title(); // get title
+      Elements paragraphs = document.getElementsByTag("p"); // first get all paragraphs
+      for (Element paragraph : paragraphs) {
+            D.paragrahs.add(paragraph.text());
+      }
+      Elements h1 = document.getElementsByTag("h1"); // get all h1
+      for (Element h : h1) {
+            D.headings.add(h.text());
+      }
+      Elements h2 = document.getElementsByTag("h2"); // get all h2
+      for (Element h : h2) {
+            D.headings.add(h.text());
+      }
+      Elements h3 = document.getElementsByTag("h3");  // get all h3
+      for (Element h : h3) {
+            D.headings.add(h.text());
+      }
+      Elements h4 = document.getElementsByTag("h4");  // get all h4
+      for (Element h : h4) {
+            D.headings.add(h.text());
+      }
+      return D;
+   }
+   static DocumentClass htmlLinkTODoc(String Link) throws IOException
+   {
+       //this function like the funntion ??? ?????  but take the link of the document
+      DocumentClass D = new DocumentClass();
+      Document document = Jsoup.connect(Link).get();
+      D.title=document.title();
+      Elements paragraphs = document.getElementsByTag("p");
+      for (Element paragraph : paragraphs) {
+            D.paragrahs.add(paragraph.text());
+      }
+      Elements h1 = document.getElementsByTag("h1");
+      for (Element h : h1) {
+            D.headings.add(h.text());
+      }
+      Elements h2 = document.getElementsByTag("h2");
+      for (Element h : h2) {
+            D.headings.add(h.text());
+      }
+      Elements h3 = document.getElementsByTag("h3");
+      for (Element h : h3) {
+            D.headings.add(h.text());
+      }
+      Elements h4 = document.getElementsByTag("h4");
+      for (Element h : h4) {
+            D.headings.add(h.text());
+      }
+      return D;
+   }
    static void test()
     {
         indexer idx = new indexer();
@@ -104,7 +177,7 @@ public class InvertedIndex {
         arr.add(tmp);
         for(int i = 0;i < arr.size(); i++)
         {
-            Document d1 = new Document(arr.get(i),i+1);
+            DocumentClass d1 = new DocumentClass(arr.get(i),i+1);
             idx.fillDoc(d1);
         }
         idx.print();
