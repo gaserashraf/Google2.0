abf324494ceb161dd149b7b909dab6edc17ada14
unknown <yousefatefelshabrawy@gmail.com>
2021-05-21 20:07:55 +0200

    Adding the database to the indexer

---
+++ db/db.sql
@@ -0,0 +1,26 @@
+CREATE TABLE words (
+word VARCHAR(15) ,
+df INT , 
+PRIMARY KEY(word)
+);
+
+CREATE TABLE wordDocs (
+word VARCHAR(15) , 
+docIndex INT ,
+tf INT,
+type VARCHAR(15) , 
+PRIMARY KEY(word, docIndex) ,
+FOREIGN KEY (word) REFERENCES words (word),
+FOREIGN KEY (docIndex) REFERENCES docLink (docIndex)
+);
+
+CREATE TABLE docLink (
+link VARCHAR(500), 
+docIndex INT, 
+PRIMARY KEY(docIndex)
+);
+CREATE TABLE searchWords (
+word VARCHAR(500), 
+PRIMARY KEY(word)
+);
+
