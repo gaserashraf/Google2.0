4384e0e1294cfeb707d4e20373f7da609be3ceb2
unknown <yousefatefelshabrawy@gmail.com>
2021-06-07 21:18:21 +0200

    fixing a pagination pug

--- frontend/src/components/Search/Pagination.js
+++ frontend/src/components/Search/Pagination.js
@@ -1,33 +1,37 @@
-import React,{useState,useContext} from 'react'
+import React, { useState, useContext } from 'react'
 import GoogleContext from "../../contexts/google/googleContext";
 
 const Pagination = () => {
 
-  let googleContext = useContext(GoogleContext);
-  let postsPerPage= googleContext.postsPerPage;
-  let totalPosts=googleContext.searchResults.length; 
-  const pageNumbers = [];
+    let googleContext = useContext(GoogleContext);
+    let postsPerPage = googleContext.postsPerPage;
+    let totalPosts = googleContext.searchResults.length;
+    const pageNumbers = [];
 
-  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
-    pageNumbers.push(i);
-  }
-  const changePage = (e)=>{
-    googleContext.setCurrentPage(e.target.textContent);
+    for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+        pageNumbers.push(i);
+    }
+    const changePage = (e) => {
+        googleContext.setCurrentPage(e.target.textContent);
 
-  }
-  return (
-    <nav>
-      <ul className='pagination'>
-        {pageNumbers.map(number => (
-          <li key={number} className={googleContext.currentPage==number ? 'active page-item' :'page-item'}>
-            <a onClick={changePage}  href='!#' className='page-link'>
-              {number}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </nav>
-  );
+    }
+    return ( <
+        nav >
+        <
+        ul className = 'pagination' > {
+            pageNumbers.map(number => ( <
+                li key = { number }
+                className = { googleContext.currentPage == number ? 'active page-item' : 'page-item' } >
+                <
+                span onClick = { changePage }
+                className = 'page-link' > { number } <
+                /span> <
+                /li>
+            ))
+        } <
+        /ul> <
+        /nav>
+    );
 };
 
 export default Pagination;
